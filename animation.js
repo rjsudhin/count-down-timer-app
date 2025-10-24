7d2081507ac07bd8d4e930dfe67808672fd6b4cc
SudhinRj <rjsudhin@users.noreply.github.com>
2025-10-24 12:20:42 +0530

    vantajs. animation added in full body

---
+++ animation.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.TOPOLOGY({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
+})
